790f2e243b3d7dd134c295a39e1a25d837a70b4d
Ana Luísa Dias <analudias@live.com>
2022-07-18 20:34:58 -0300

    first commit

---
+++ src/main/java/com/analudias/moviechooser/MoviechooserApplication.java
@@ -0,0 +1,13 @@
+package com.analudias.moviechooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviechooserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviechooserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analudias/moviechooser/MoviechooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analudias.moviechooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviechooserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
